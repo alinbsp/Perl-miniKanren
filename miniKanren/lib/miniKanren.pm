b3c3b92a7cf22bf35745d81c4e1ebf51157e1d85
Alin Iacob <alinbsp@gmail.com>
2016-03-24 13:53:17 +0200

    Vars should be vectors

--- miniKanren/lib/miniKanren.pm
+++ miniKanren/lib/miniKanren.pm
@@ -3,10 +3,14 @@ use warnings;
 package miniKanren;
 
 use base 'Exporter';
-our @EXPORT = qw(run);
+our @EXPORT = qw(run var);
 
 sub run {
+}
 
+sub var {
+  my @var = shift;
+  return @var;
 }
 
 1;
