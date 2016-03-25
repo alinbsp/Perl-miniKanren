08eb56a9fdc55b01b42cd4d68f68b47d764f2706
Alin Iacob <alinbsp@gmail.com>
2016-03-25 16:33:49 +0200

    The most naive version of fresh

--- miniKanren/lib/miniKanren.pm
+++ miniKanren/lib/miniKanren.pm
@@ -5,7 +5,7 @@ use 5.014;
 package miniKanren;
 
 use base 'Exporter';
-our @EXPORT = qw(run var append);
+our @EXPORT = qw(run var append fresh eql true false);
 
 use constant false => 0;
 use constant true => 1;
@@ -29,8 +29,17 @@ sub append {
   return @_;
 }
 
+# my $x = fresh();
+# my ($x, $y) = fresh(2);
 sub fresh {
+  return [] unless $_[0];
 
+  my @fresh = ();
+  for my $i (1..$_[0]) {
+    push @fresh, [];
+  }
+  return @fresh;
 }
 
+sub eql {}
 1;
